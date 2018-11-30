d3a6903e8d156d8166c90689f29ea836c537fa04
Benoit Blanchon <github@benoitblanchon.fr>
2018-11-30 12:16:14 +0100

    Remove convertion from int to size_t

--- test/JsonArray/get.cpp
+++ test/JsonArray/get.cpp
@@ -10,10 +10,6 @@ TEST_CASE("JsonArray::get()") {
   deserializeJson(doc, "[1,2,3]");
   JsonArray array = doc.as<JsonArray>();
 
-  SECTION("Underflow") {
-    REQUIRE(array.get(-1).isNull());
-  }
-
   SECTION("Overflow") {
     REQUIRE(array.get(3).isNull());
   }
